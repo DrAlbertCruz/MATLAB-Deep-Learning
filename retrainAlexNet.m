e1b81fd37c4f6c08266de2381f84ae086eed056a
Albert <acruz@csub.edu>
2018-07-10 11:02:13 -0700

    Daily

---
+++ retrainAlexNet.m
@@ -0,0 +1,80 @@
+function net = retrainAlexNet( trainingData, varargin )
+load default                                            % Load defaults
+numClasses = length(unique( trainingData.Labels ));     % Num classes
+%% Input validation
+if ~mod( nargin, 2 )
+    error( default.msgNumArgs );
+end
+%% Default arguments
+EPOCH = default.EPOCH;
+MINIBATCH_SIZE = default.MINIBATCH_SIZE;
+INITIAL_LEARNING_RATE = default.INITIAL_LEARNING_RATE;
+L2_REGULARIZATION = default.L2_REGULARIZATION;
+FLAG_SHUFFLE = default.FLAG_SHUFFLE;
+VERBOSE_FREQUENCY = default.VERBOSE_FREQUENCY;
+FLAG_GPU = default.FLAG_GPU;
+for i=1:2:length(varargin)
+    arg_ = cell2mat(varargin(i));
+    val_ = cell2mat(varargin(i+1));
+    switch arg_
+        case 'epoch'
+            EPOCH = val_;
+        case 'miniBatchSize'
+            MINIBATCH_SIZE = val_;
+        case 'initialLearningRate'
+            INITIAL_LEARNING_RATE = val_;
+        case 'L2Regularization'
+            L2_REGULARIZATION = val_;
+        case 'shuffle'
+            FLAG_SHUFFLE = val_;
+        case 'verbosity'
+            VERBOSE_FREQUENCY = val_;
+        case 'gpu'
+            FLAG_GPU = val_;
+        otherwise
+            error( 'Invalid vararg pair!' );
+    end
+end
+%% Switches for converting flags to strings if needed
+% Set the flag to shuffle the data on each epoch. Unused: 'once'.
+if FLAG_SHUFFLE == true
+    SHUFFLE = 'every-epoch';
+else
+    SHUFFLE = 'never';
+end
+% Set the flag to use the gpu or the cpu. Unused: multi-worker
+if FLAG_GPU == true
+    GPU = 'gpu';
+else
+    GPU = 'cpu';
+end
+%% Set training options
+opts = trainingOptions( default.optimizer, ...
+    'InitialLearnRate', INITIAL_LEARNING_RATE, ...
+    'L2Regularization', L2_REGULARIZATION, ...
+    'MaxEpochs', EPOCH, ...
+    'MiniBatchSize', MINIBATCH_SIZE, ...
+    'Verbose', true, ...
+    'VerboseFrequency', VERBOSE_FREQUENCY, ...
+    'Shuffle', SHUFFLE, ...
+    'ExecutionEnvironment', GPU ...
+    );
+%% Begin loading AlexNet, replace final classification layer
+alex = alexnet;
+layers = alex.Layers;
+myFCName = 'acc_fcout';
+finalFCLayer = fullyConnectedLayer(numClasses,'Name',myFCName);
+finalFCLayerInputSize = 4096;
+finalFCLayer.Weights = gpuArray(single(randn([numClasses finalFCLayerInputSize])*0.0001));
+finalFCLayer.Bias = gpuArray(single(randn([numClasses 1])*0.0001));
+finalFCLayer.WeightLearnRateFactor = default.WeightLearnRateFactor;
+finalFCLayer.WeightL2Factor = default.WeightL2Factor;
+finalFCLayer.BiasLearnRateFactor = default.BiasLearnRateFactor;
+finalFCLayer.BiasL2Factor = default.BiasL2Factor;
+endOfNet = 22;
+layers(1:endOfNet) = freezeWeights(layers(1:endOfNet));
+layers(23) = finalFCLayer;
+layers(24) = softmaxLayer('Name','acc_fcout_softmax');
+layers(25) = classificationLayer('Name','acc_fcout_output');
+%% Training step
+net = trainNetwork(trainingData,layers,opts);
--- thisJob.m
+++ thisJob.m
@@ -4,9 +4,15 @@ catch e
     display( "No parpool open" );
 end
 pool = parpool(3);
-parfeval( pool, @retrainDeepLearner, 0, 1:40, '~/data/Salento-Grapevine-Yellows-Dataset/raw', '/home/acruz/MATLAB-Deep-Learning/Salento2', 'InceptionResnetV2_e', @retrainInceptionResnetV2, 299 );
-parfeval( pool, @retrainDeepLearner, 0, 1:40, '~/data/Salento-Grapevine-Yellows-Dataset/raw', '/home/acruz/MATLAB-Deep-Learning/Salento2', 'AlexNet_e', @retrainAlexNet, 227 );
-parfeval( pool, @retrainDeepLearner, 0, 13:40, '~/data/Salento-Grapevine-Yellows-Dataset/raw', '/home/acruz/MATLAB-Deep-Learning/Salento2', 'ResNet50_e', @retrainResNet50, 224 );
-parfeval( pool, @retrainDeepLearner, 0, 22:40, '~/data/Salento-Grapevine-Yellows-Dataset/raw', '/home/acruz/MATLAB-Deep-Learning/Salento2', 'GoogLeNet_e', @retrainGoogLenet, 224 );
-parfeval( pool, @retrainDeepLearner, 0, 24:40, '~/data/Salento-Grapevine-Yellows-Dataset/raw', '/home/acruz/MATLAB-Deep-Learning/Salento2', 'AlexNet_e', @retrainSqueezeNet, 227 );
-parfeval( pool, @retrainDeepLearner, 0, 8:40, '~/data/Salento-Grapevine-Yellows-Dataset/raw', '/home/acruz/MATLAB-Deep-Learning/Salento2', 'ResNet101_e', @retrainResNet101, 224 );
+F1 = parfeval( pool, @retrainDeepLearner, 0, 1:40, '~/data/Salento-Grapevine-Yellows-Dataset/raw', '/home/acruz/MATLAB-Deep-Learning/Salento3', 'InceptionResnetV2_e', @retrainInceptionResnetV2, 299 );
+%F2 = parfeval( pool, @retrainDeepLearner, 0, 1:40, '~/data/Salento-Grapevine-Yellows-Dataset/raw', '/home/acruz/MATLAB-Deep-Learning/Salento3', 'AlexNet_e', @retrainAlexNet, 227 );
+%F3 = parfeval( pool, @retrainDeepLearner, 0, 1:40, '~/data/Salento-Grapevine-Yellows-Dataset/raw', '/home/acruz/MATLAB-Deep-Learning/Salento3', 'ResNet50_e', @retrainResNet50, 224 );
+F4 = parfeval( pool, @retrainDeepLearner, 0, 1:40, '~/data/Salento-Grapevine-Yellows-Dataset/raw', '/home/acruz/MATLAB-Deep-Learning/Salento3', 'GoogLeNet_e', @retrainGoogLenet, 224 );
+F5 = parfeval( pool, @retrainDeepLearner, 0, 1:40, '~/data/Salento-Grapevine-Yellows-Dataset/raw', '/home/acruz/MATLAB-Deep-Learning/Salento3', 'SqueezeNet_e', @retrainSqueezeNet, 227 );
+F6 = parfeval( pool, @retrainDeepLearner, 0, 1:40, '~/data/Salento-Grapevine-Yellows-Dataset/raw', '/home/acruz/MATLAB-Deep-Learning/Salento3', 'ResNet101_e', @retrainResNet101, 224 );
+F7 = parfeval( pool, @retrainDeepLearner, 0, 1:40, '~/data/Salento-Grapevine-Yellows-Dataset/localized', '/home/acruz/MATLAB-Deep-Learning/localized', 'InceptionResnetV2_e', @retrainInceptionResnetV2, 299 );
+F8 = parfeval( pool, @retrainDeepLearner, 0, 1:40, '~/data/Salento-Grapevine-Yellows-Dataset/localized', '/home/acruz/MATLAB-Deep-Learning/localized', 'AlexNet_e', @retrainAlexNet, 227 );
+F9 = parfeval( pool, @retrainDeepLearner, 0, 1:40, '~/data/Salento-Grapevine-Yellows-Dataset/localized', '/home/acruz/MATLAB-Deep-Learning/localized', 'ResNet50_e', @retrainResNet50, 224 );
+F10 = parfeval( pool, @retrainDeepLearner, 0, 1:40, '~/data/Salento-Grapevine-Yellows-Dataset/localized', '/home/acruz/MATLAB-Deep-Learning/localized', 'GoogLeNet_e', @retrainGoogLenet, 224 );
+F11 = parfeval( pool, @retrainDeepLearner, 0, 1:40, '~/data/Salento-Grapevine-Yellows-Dataset/localized', '/home/acruz/MATLAB-Deep-Learning/localized', 'SqueezeNet_e', @retrainSqueezeNet, 227 );
+F12 = parfeval( pool, @retrainDeepLearner, 0, 1:40, '~/data/Salento-Grapevine-Yellows-Dataset/localized', '/home/acruz/MATLAB-Deep-Learning/localized', 'ResNet101_e', @retrainResNet101, 224 );
