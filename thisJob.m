9cbcfc3aaa0083eb4849eb5a34efe315afc02472
Albert <acruz@csub.edu>
2018-07-14 10:33:23 -0700

    daily

--- thisJob.m
+++ thisJob.m
@@ -4,21 +4,9 @@ catch e
     display( "No parpool open" );
 end
 pool = parpool(3);
-%F1 = parfeval( pool, @retrainDeepLearner, 0, 1:40,
-%'~/data/Salento-Grapevine-Yellows-Dataset/raw',
-%'/home/acruz/MATLAB-Deep-Learning/Salento3', 'InceptionResnetV2_e',
-%@retrainInceptionResnetV2, 299 ); Cancelled
-%F2 = parfeval( pool, @retrainDeepLearner, 0, 1:40, '~/data/Salento-Grapevine-Yellows-Dataset/raw', '/home/acruz/MATLAB-Deep-Learning/Salento3', 'AlexNet_e', @retrainAlexNet, 227 );
-%F3 = parfeval( pool, @retrainDeepLearner, 0, 1:40, '~/data/Salento-Grapevine-Yellows-Dataset/raw', '/home/acruz/MATLAB-Deep-Learning/Salento3', 'ResNet50_e', @retrainResNet50, 224 );
-%F4 = parfeval( pool, @retrainDeepLearner, 0, 1:40, '~/data/Salento-Grapevine-Yellows-Dataset/raw', '/home/acruz/MATLAB-Deep-Learning/Salento3', 'GoogLeNet_e', @retrainGoogLenet, 224 );
-%F5 = parfeval( pool, @retrainDeepLearner, 0, 1:40, '~/data/Salento-Grapevine-Yellows-Dataset/raw', '/home/acruz/MATLAB-Deep-Learning/Salento3', 'SqueezeNet_e', @retrainSqueezeNet, 227 );
-%F6 = parfeval( pool, @retrainDeepLearner, 0, 1:40, '~/data/Salento-Grapevine-Yellows-Dataset/raw', '/home/acruz/MATLAB-Deep-Learning/Salento3', 'ResNet101_e', @retrainResNet101, 224 );
-% F7 = parfeval( pool, @retrainDeepLearner, 0, 1:40,
-% '~/data/Salento-Grapevine-Yellows-Dataset/localized',
-% '/home/acruz/MATLAB-Deep-Learning/localized', 'InceptionResnetV2_e',
-% @retrainInceptionResnetV2, 299 ); %Exception, cancelled
-%F8 = parfeval( pool, @retrainDeepLearner, 0, 1:40, '~/data/Salento-Grapevine-Yellows-Dataset/localized', '/home/acruz/MATLAB-Deep-Learning/localized', 'AlexNet_e', @retrainAlexNet, 227 );
-F9 = parfeval( pool, @retrainDeepLearner, 0, 1:40, '~/data/Salento-Grapevine-Yellows-Dataset/localized', '/home/acruz/MATLAB-Deep-Learning/localized', 'ResNet50_e', @retrainResNet50, 224 );
-%F10 = parfeval( pool, @retrainDeepLearner, 0, 1:40, '~/data/Salento-Grapevine-Yellows-Dataset/localized', '/home/acruz/MATLAB-Deep-Learning/localized', 'GoogLeNet_e', @retrainGoogLenet, 224 );
-%F11 = parfeval( pool, @retrainDeepLearner, 0, 1:40, '~/data/Salento-Grapevine-Yellows-Dataset/localized', '/home/acruz/MATLAB-Deep-Learning/localized', 'SqueezeNet_e', @retrainSqueezeNet, 227 );
-F12 = parfeval( pool, @retrainDeepLearner, 0, 1:40, '~/data/Salento-Grapevine-Yellows-Dataset/localized', '/home/acruz/MATLAB-Deep-Learning/localized', 'ResNet101_e', @retrainResNet101, 224 );
+
+alexnet = parfeval( pool, @retrainDeepLearner, 0, 1:40, '~/data/Salento-Grapevine-Yellows-Dataset/localized', '/home/acruz/MATLAB-Deep-Learning/localized2', 'AlexNet_e', @retrainAlexNet, 227 );
+resnet50 = parfeval( pool, @retrainDeepLearner, 0, 1:40, '~/data/Salento-Grapevine-Yellows-Dataset/localized', '/home/acruz/MATLAB-Deep-Learning/localized2', 'ResNet50_e', @retrainResNet50, 224 );
+squeezenet = parfeval( pool, @retrainDeepLearner, 0, 1:40, '~/data/Salento-Grapevine-Yellows-Dataset/localized', '/home/acruz/MATLAB-Deep-Learning/localized2', 'SqueezeNet_e', @retrainSqueezeNet, 227 );
+resnet101 = parfeval( pool, @retrainDeepLearner, 0, 1:40, '~/data/Salento-Grapevine-Yellows-Dataset/localized', '/home/acruz/MATLAB-Deep-Learning/localized2', 'ResNet101_e', @retrainResNet101, 224 );
+googlenet = parfeval( pool, @retrainDeepLearner, 0, 1:40, '~/data/Salento-Grapevine-Yellows-Dataset/localized', '/home/acruz/MATLAB-Deep-Learning/localized2', 'GoogLeNet_e', @retrainGoogLenet, 224 );
